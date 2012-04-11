9563b0850c17c50870ae00dd8356fa4abba4fd4d
Alex Yule <alexy@codeforamerica.org>
2012-04-11 14:25:53 -0700

    removed some block syntax

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,24 +1,26 @@
 FactoryGirl.define do
-  #   attr_accessible :end_date, :phone, :start_date, :text, :title, :poll_type
+  
   factory :poll do
     start_date { Time.now }
     end_date { Time.now + 1.week }
-    phone { '9091234567' }
-    text { 'Where do you buy your groceries?' }
-    title { 'Groceries' }
+    phone '19093334444'
+    text 'Where do you buy your groceries?'
+    title 'Groceries'
+    poll_type 'OPEN'
     
     factory :poll_multi do
       choices {{ "a" => "Wal-mart", "b" => "Bi-rite", "c" => "Bodega"}}
-      poll_type { 'MULTI' }
+      poll_type 'MULTI'
     end
     
     factory :poll_open do
-      poll_type { 'OPEN' }
+      poll_type 'OPEN' 
     end
     
     factory :poll_ended do
       start_date { Time.now - 1.week }
       end_date { Time.now - 3.days }
     end
+
   end
 end
