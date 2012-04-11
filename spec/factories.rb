2ed2d9d689612e08e1b5f469bc520833b78fdb54
Alex Yule <alexy@codeforamerica.org>
2012-04-10 19:11:15 -0700

    added poll factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,8 +4,21 @@ FactoryGirl.define do
     start_date { Time.now }
     end_date { Time.now + 1.week }
     phone { '9091234567' }
-    poll_type { 'OPEN' }
     text { 'Where do you buy your groceries?' }
     title { 'Groceries' }
+    
+    factory :poll_multi do
+      choices {{ "a" => "Wal-mart", "b" => "Bi-rite", "c" => "Bodega"}}
+      poll_type { 'MULTI' }
+    end
+    
+    factory :poll_open do
+      poll_type { 'OPEN' }
+    end
+    
+    factory :poll_ended do
+      start_date { Time.now - 1.week }
+      end_date { Time.now - 3.days }
+    end
   end
 end
