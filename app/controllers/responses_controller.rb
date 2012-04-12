4ecfffabe50f23642ea2d637d983073810d2221f
Alex Yule <alexy@codeforamerica.org>
2012-04-12 11:27:33 -0700

    created responses controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,3 +1,10 @@
 class ResponsesController < ApplicationController
 
+  #post /responses/receive_message
+  def receive_message
+    puts params
+    if params[:session]
+      puts params[:session]
+    end
+  end
 end
