31d8426cfbb87e62e5b2df9763f0a8b2e87bc65f
Alex Yule <alexy@codeforamerica.org>
2012-04-12 17:42:06 -0700

    cleaned up poll controller tests

--- spec/controllers/polls_controller_spec.rb
+++ spec/controllers/polls_controller_spec.rb
@@ -3,63 +3,15 @@ require 'spec_helper'
 describe PollsController do
   #, "#show" do
   before(:each) do
-    #@current_user = login
-    controller.stub(:get_new_phone_number).and_return("4443332222")
     @poll=FactoryGirl.create(:poll) #:user=>controller.current_user
   end
   
-
-  # describe "authorization" do
-  #   pending "all actions should be accessible to logged in users" do
-  #   end
-  # 
-  #   describe "no actions should be accessible to non-logged-in users and should redirect to signin" do
-  #     before(:each) do
-  #       sign_out(@current_user)
-  #     end
-  # 
-  #     it "on the show page should redirect to signin page" do
-  #       get :show, {:id => @group.id}
-  #       response.should redirect_to(new_user_session_path)
-  #     end
-  # 
-  #     it "on the edit page should redirect to signin page" do
-  #       get :edit, {:id => @group.id}
-  #       response.should redirect_to(new_user_session_path)
-  #     end
-  # 
-  #     it "on the index page should redirect to signin page" do
-  #       get :index
-  #       response.should redirect_to(new_user_session_path)
-  #     end
-  # 
-  #     it "on the new page should redirect to signin page" do
-  #       get :new
-  #       response.should redirect_to(new_user_session_path)
-  #     end
-  #   end
-  # 
-  # end
-  # 
-  
   describe "#create" do
-    # before :each do
-    #   login
-    # end
     it "after successful create, should redirect to show page" do
-      controller.should_receive(:get_new_phone_number).and_return("5556667777")
-      post :create, :poll=>{}
-      assigns[:poll].should_not be_nil
+      post :create, :poll=> FactoryGirl.attributes_for(:poll)
+      assigns[:poll].should_not be_nil # :poll should exist
       response.should redirect_to(assigns[:poll])
     end
-  end
-    
-    # it "must belong to the logged in user" do
-    #   post :create, :poll=>{}
-    #   assigns[:poll].user.should == controller.current_user
-    #   post :create, :poll=>{:user_id=>999}
-    #   assigns[:poll].user.should == controller.current_user
-    # end
   end
   
   describe "boilerplate stuff" do
@@ -68,23 +20,17 @@ describe PollsController do
         get :index
         assigns[:polls].should_not be_nil
       end
-      pending "should be limited to logged in users' polls only" do
-      end
     end
+  end
     
-    describe "show/edit" do
-      it "should set @poll and a @page_title" do
-        [:show,:edit].each do |meth|
-          get meth, {:id=>@poll.id}
-          assigns[:poll].should_not be_nil
-          assigns[:page_title].should_not be_nil
-        end
+  describe "show/edit" do
+    it "should set @poll and a @page_title" do
+      [:show,:edit].each do |meth|
+        get meth, {:id=>@poll.id}
+        assigns[:poll].should_not be_nil
+        assigns[:page_title].should_not be_nil
       end
     end
-  # it "must belong to the logged in user" do
-  #   post :create, :group=>{}
-  #   assigns[:group].user.should == controller.current_user
-  #   post :create, :group=>{:user_id=>999}
-  #   assigns[:group].user.should == controller.current_user
-  # end
+  end
+
 end
