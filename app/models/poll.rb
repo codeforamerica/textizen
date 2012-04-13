b72646b2fe72680717c166752d738ec462acec71
Alex Yule <alexy@codeforamerica.org>
2012-04-12 17:35:49 -0700

    poll model updates: now supports provisioning phone number WITH TESTS!

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -5,5 +5,39 @@ class Poll < ActiveRecord::Base
   
   validates :poll_type, :inclusion => { :in => %w(MULTI OPEN), :message => "%{value} is not a valid poll type" }  
   validates_uniqueness_of :phone
+  before_create :set_new_phone_number
+  before_destroy :destroy_phone_number
+
+  def set_new_phone_number
+    puts 'set new phone number'
+    self.phone = self.phone || get_phone_number
+  end
+
+  # recursive function to get a new phone number, making sure that it wasn't previously assigned
+  # if the number was assigned to a previous (closed) poll, then it calls itself again
+  # once a non-duplicate number is found, all duplicates are destroyed
+  def get_phone_number(addresses_to_clear = [])
+    puts 'get phone number'
+    tp = TropoProvisioning.new(ENV['TROPO_USERNAME'], ENV['TROPO_PASSWORD'])
+    address = tp.create_address(ENV['TROPO_APP_ID'], { :type => 'number', :prefix => '1415' })
+    unless Poll.where(:phone=>address['address']).empty?
+      addresses_to_clear.push(address['address'])
+      return get_phone_number(addresses_to_clear)
+    else
+      addresses_to_clear.each do |a|
+        destroy_phone_number(a)
+      end
+    end
+    puts address['address']
+    return address['address']
+  end
+
+  def destroy_phone_number(phone = '')
+    phone = phone || self.phone
+    puts 'destroy phone number'
+    tp = TropoProvisioning.new(ENV['TROPO_USERNAME'], ENV['TROPO_PASSWORD'])
+    tp.delete_address(ENV['TROPO_APP_ID'], phone)
+  end
+
 
 end
--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe Poll do
-  before(:each) do
-    FactoryGirl.create(:poll)
-  end
+
   
   it { should have_many(:responses) }
   it { should belong_to(:user) }
@@ -12,5 +10,24 @@ describe Poll do
   it { should allow_value("OPEN").for(:poll_type) }
   it { should_not allow_value("foo").for(:poll_type) }
 
-  it { should validate_uniqueness_of(:phone) }
+  describe "check phone assignment and uniqueness" do
+
+    before(:each) do
+      FactoryGirl.create(:poll)
+    end
+    
+    it { should validate_uniqueness_of(:phone) }
+    
+    it "should be assigned a tropo phone number if not already assigned" do
+      Poll.stub(:get_phone_number).and_return("+14153334444")
+      @poll = Poll.create(:poll_type=>'OPEN')
+      @poll.phone.should == "+14153334444"
+    end
+
+    it "should be assigned a passed-in phone number" do
+      @poll = Poll.create(:poll_type=>'OPEN', :phone=>"+14151112222")
+      @poll.phone.should == "+14151112222"
+    end
+
+  end
 end
