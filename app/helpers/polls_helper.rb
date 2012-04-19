4a2c25c607e0c73c44bb1cf2a924fd770ec7a2e8
Alex Yule <alexy@codeforamerica.org>
2012-04-19 15:51:27 -0700

    added nice poll type formatting

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,2 +1,11 @@
 module PollsHelper
+  def format_poll_type(type)
+    case type
+    when 'OPEN'
+      return 'Open'
+    when 'MULTI'
+      return 'Multiple Choice'
+    end
+    return ''
+  end
 end
