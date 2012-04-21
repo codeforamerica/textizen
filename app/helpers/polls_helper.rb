9eb55b6131c97368c597cf28137e6be98c231f8b
Alex Yule <alexy@codeforamerica.org>
2012-04-21 01:05:53 -0700

    more sparkline stuff == formatting

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -10,6 +10,6 @@ module PollsHelper
   end
 
   def sparkline(data, width=100, height=40, style='', _class='')
-    return image_tag(Gchart.line(:data=>data, :width=>width, :height=>height), :style=>style, :class=>_class)
+    return image_tag(Gchart.sparkline(:data=>data, :width=>width, :height=>height, :bg=>'00000000'), :style=>style, :class=>_class)
   end
 end
