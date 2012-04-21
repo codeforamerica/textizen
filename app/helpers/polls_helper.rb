b0b24323e5b332373d56febd519cb43ae190b1e3
Alex Yule <alexy@codeforamerica.org>
2012-04-21 02:22:10 -0700

    added date formatter for use with time-since-last-response WITH TESTS! fixed tests to pass after changes to sparkline and poll_type formatter

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -12,4 +12,17 @@ module PollsHelper
   def sparkline(data, width=100, height=40, style='', _class='')
     return image_tag(Gchart.sparkline(:data=>data, :width=>width, :height=>height, :bg=>'00000000'), :style=>style, :class=>_class)
   end
+
+  #formats seconds into days, minutes, etc
+  def format_time(secs)
+    if secs < 60
+      return pluralize(secs.round, 'second')
+    elsif secs < 60 * 60
+      return pluralize((secs/60).round, 'minute')
+    elsif secs < 60 * 60 * 24
+      return pluralize((secs/60/60).round, 'hour')
+    else
+      return pluralize((secs/60/60/24).round, 'day')
+    end
+  end
 end
--- spec/helpers/polls_helper_spec.rb
+++ spec/helpers/polls_helper_spec.rb
@@ -14,7 +14,7 @@ describe PollsHelper do
   describe "format_poll_type" do
     it "should return Open" do
       result = format_poll_type('OPEN')
-      result.should == 'Open'
+      result.should == 'Open Answer'
     end
     it "should return Multiple Choice" do
       result = format_poll_type('MULTI')
@@ -31,9 +31,45 @@ describe PollsHelper do
       @data = [10, 10, 4, 8, 12, 7, 7, 2, 0]
       @width = 100
       @height = 40
-      @expected = "<img alt=\"Chart?chd=s:zzup9kkka&amp;cht=lc&amp;chs=100x40&amp;chxr=0,10,12\" class=\"\" src=\"http://chart.apis.google.com/chart?chd=s:zzUp9kkKA&amp;cht=lc&amp;chs=100x40&amp;chxr=0,10,12\" style=\"\" />"
+      @expected = "<img alt=\"Chart?chf=bg,s,00000000&amp;chd=s:zzup9kkka&amp;cht=ls&amp;chs=100x40&amp;chxr=0,10,12\" class=\"\" src=\"http://chart.apis.google.com/chart?chf=bg,s,00000000&amp;chd=s:zzUp9kkKA&amp;cht=ls&amp;chs=100x40&amp;chxr=0,10,12\" style=\"\" />"
       puts sparkline(@data, @width, @height)
       sparkline(@data, @width, @height).should == @expected
     end
   end
+
+  describe "date helper" do
+    it "should parse as seconds" do
+      result = format_time(59)
+      result.should == "59 seconds"
+    end
+    it "should pluralize seconds" do
+      result = format_time(1.43)
+      result.should == "1 second"
+    end
+    it "shuld parse as minutes" do
+      result = format_time(123)
+      result.should == "2 minutes"
+    end
+    it "shuld pluralize minutes" do
+      result = format_time(89)
+      result.should == "1 minute"
+    end
+    it "should parse as hours" do
+      result = format_time(60*60*3 + 5.122)
+      result.should == "3 hours"
+    end
+    it "should pluralize hours" do
+      result = format_time(60*60 + 5.122)
+      result.should == "1 hour"
+    end
+    it "should parse as days" do
+      result = format_time(60*60*24*2 + 5.122)
+      result.should == "2 days"
+    end
+    it "should pluraliz days" do
+      result = format_time(60*60*24*1 + 5.122)
+      result.should == "1 day"
+    end
+  end
+
 end
