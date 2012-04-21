3043911d63c39259b91829fac434559e58dd175a
Alex Yule <alexy@codeforamerica.org>
2012-04-21 01:44:13 -0700

    open --> open answer

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -2,7 +2,7 @@ module PollsHelper
   def format_poll_type(type)
     case type
     when 'OPEN'
-      return 'Open'
+      return 'Open Answer'
     when 'MULTI'
       return 'Multiple Choice'
     end
