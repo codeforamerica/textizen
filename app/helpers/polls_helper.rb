8c4fba21d4c79ab7794017f388eecb53871a8017
Michelle Lee <mlee@codeforamerica.org>
2012-04-24 11:23:21 -0400

    change word count block to table

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -30,9 +30,11 @@ module PollsHelper
   def html_histogram(histogram)
     result = '' 
     if histogram
+      result << "<table>"
       histogram.slice(0,7).each do |h|
-        result << "<div style='font-size:#{h[1]/8.0}em' class='histogram-pair'><span class='histogram-word'>#{h[0]}</span><span class='histogram-separator'>:</span><span class='histogram-count'>#{h[1]}</span></div>\n"
+        result << "<tr style='font-size:#{h[1]/8.0}em' class='histogram-pair'><td class='histogram-word'>#{h[0]}</td><td class='histogram-separator'>:</td><td class='histogram-count'>#{h[1]}</td></tr>\n"
       end
+      result << "</table>"
     end
     return raw result
   end
