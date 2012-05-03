4696af0119a21483991e3a7c520c3c9287d0994a
Alex Yule <alexy@codeforamerica.org>
2012-05-03 14:39:35 -0700

    validate presence of q type and poll id

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,8 @@ describe Question do
   it { should have_one(:next_question) }
   it { should belong_to(:poll) }
 
+  it { should validate_presence_of(:poll_id) }
+  it { should validate_presence_of(:question_type) }
   it { should allow_value("MULTI").for(:question_type) }
   it { should allow_value("OPEN").for(:question_type) }
   it { should allow_value("YN").for(:question_type) }
