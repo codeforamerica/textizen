2c72b11ba4d6dd6b75c06812ae99038bc9b50f04
Alex Yule <alexy@codeforamerica.org>
2012-05-03 14:22:13 -0700

    added next_question and YN to question model, tests passing

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,9 @@
 class Question < ActiveRecord::Base
   attr_accessible :next_question_id, :options, :poll_id, :question_type, :text
   has_many :responses
+  has_one :next_question, :class_name => "Question", :foreign_key => "next_question_id"
   belongs_to :poll
 
-  validates :question_type, :inclusion => { :in => %w(MULTI OPEN), :message => "%{value} is not a valid poll type" }  
+  validates :question_type, :inclusion => { :in => %w(MULTI OPEN YN), :message => "%{value} is not a valid poll type" }  
  
 end
