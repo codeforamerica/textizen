1435af965cf337131c1bd8c8527abbc119c050cb
Alex Yule <alexy@codeforamerica.org>
2012-05-04 01:18:18 -0700

    added nesting to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,9 @@
 class Question < ActiveRecord::Base
   attr_accessible :poll_id, :question_type, :text, :parent_option_id
   has_many :responses
-  has_many :options
+  has_many :options, :dependent => :destroy
+  accepts_nested_attributes_for :options, :reject_if => lambda { |o| o[:value].blank? }, :allow_destroy => true
+
 
   belongs_to :poll
   belongs_to :option, :foreign_key => "parent_option_id"
