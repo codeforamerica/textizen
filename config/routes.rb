a85dd6c230d46ffd5ca94ea948c7c5ab29487699
Alex Yule <alexy@codeforamerica.org>
2012-05-12 22:08:12 -0700

    broken

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ TxtyourcityRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  #root :to => 'application#index' #'polls#index'
+  root :to => 'application#index' #'polls#index'
 
   # See how all your routes lay out with "rake routes"
 
