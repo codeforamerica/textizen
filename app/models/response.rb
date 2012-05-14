b88ef3fa633fd5b70f8846d8488a5d66a980c90f
Alex Yule <alexy@codeforamerica.org>
2012-05-13 20:19:00 -0700

    validate for presence of question_id for response

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,6 @@
 class Response < ActiveRecord::Base
-  attr_accessible :from, :response, :to
+  attr_accessible :from, :response, :to, :question_id
   belongs_to :question
 
-  validates_presence_of :from, :response
+  validates_presence_of :from, :response, :question_id
 end
