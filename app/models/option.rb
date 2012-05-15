893f5098cb34af81791ddbdfa52b539aea22caf1
Alex Yule <alexy@codeforamerica.org>
2012-05-14 20:14:16 -0700

    made follow_up_attributes accessible, followup creation now working

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,5 +1,5 @@
 class Option < ActiveRecord::Base
-  attr_accessible :follow_up_id, :question_id, :text, :value
+  attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes
   belongs_to :question
   has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id"
   validates_presence_of :text, :value
