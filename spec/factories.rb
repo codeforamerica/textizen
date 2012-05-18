2bd18457bcbbae57e45cf7e6c40a9e90a718cc4f
Alex Yule <alexy@codeforamerica.org>
2012-05-17 17:04:07 -0700

    adding factories for polls, questions, and options

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     end_date { Time.now + 1.week }
     phone {'1'+rand(10 ** 10).to_s}
     title 'Groceries'
+    #user
     
     factory :poll_multi do
     end
@@ -13,13 +14,17 @@ FactoryGirl.define do
     end
 
     factory :poll_yn do
-
+      after_create do |poll, evaluator|
+        FactoryGirl.create(:question_yn,  poll: poll)
+      end
     end
 
-    factory :poll_no_questions do
-
+    factory :poll_with_question do
+      after_create do |poll, evaluator|
+        FactoryGirl.create(:question,  poll: poll)
+      end
     end
-    
+
     factory :poll_ended do
       start_date { Time.now - 1.week }
       end_date { Time.now - 3.days }
@@ -29,12 +34,20 @@ FactoryGirl.define do
       phone '14842020381'
       title 'New SEPTA Stop'
     end
+
+    factory :poll_with_responses do
+      questions {|questions| [questions.association(:question_with_responses)]}
+    end
   end
 
   factory :question do
     text "Where do you buy groceries?"
     question_type 'OPEN'
     poll
+
+    factory :question_with_responses do
+
+    end
 
     factory :question_multi do
       question_type 'MULTI'
@@ -43,16 +56,32 @@ FactoryGirl.define do
 
     factory :question_yn do
       question_type 'YN'
-      text "Do you ride public transit?"
-#      options '{ "y":{"val":"Yes"}, "n": {"val":"No"} }'
+      text "Would you ride light rail along the boulevard?"
+      after_create do |question, evaluator|
+        FactoryGirl.create(:option_y, question: question) 
+        FactoryGirl.create(:option_n, question: question) 
+      end
     end
   end
 
   factory :response do
     from {'1'+rand(10 ** 10).to_s}
     response 'I buy groceries IN YOUR FACE'
-    question
   end
 
+  factory :option do
+    question
+    text 'wal-mart'
+    value 'a'
 
+    factory :option_y do
+      text 'yes'
+      value 'y'
+    end
+
+    factory :option_n do
+      text 'no'
+      value 'n'
+    end
+  end
 end
