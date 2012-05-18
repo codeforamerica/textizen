867859c275575c7eeb317096e6700dffa39efc53
Alex Yule <alexy@codeforamerica.org>
2012-05-17 17:03:59 -0700

    adding tests for yn polls, still incomplete

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -2,6 +2,75 @@ require 'spec_helper'
 
 describe ResponsesController do
   describe "POST receive_message" do
+    context "yn poll with y followup and next question" do
+      before :each do
+        @poll = FactoryGirl.create(:poll_yn, :phone=>'14153334444')
+      end
+
+      describe "valid sms y answer with no previous responses" do
+        it "should save a response and send the followup" do
+          post :receive_message, :params => TROPO_SMS_RESPONSE_Y
+          pending
+        end
+      end
+      
+      describe "valid sms n answer with no previous responses" do
+        it "should save a response and send the next question" do
+          post :receive_message, :params => TROPO_SMS_RESPONSE_N
+          pending
+        end
+      end
+
+      describe "valid sms answer with previous y response" do
+        it "should save a responses for the followup and send the next question" do
+          post :receive_message, :params => TROPO_SMS_RESPONSE_Y
+          post :receive_message, :params => TROPO_SMS_RESPONSE_OPEN
+          pending
+        end
+      end
+
+      describe "valid sms answer with previous n (non-follow-up-triggering) response" do
+        it "should save a response to the next question and say thanks" do
+          post :receive_message, :params => TROPO_SMS_RESPONSE_N
+          post :receive_message, :params => TROPO_SMS_RESPONSE_OPEN
+          pending
+        end
+      end
+
+      describe "valid sms answer with two previous responses (y and anything)" do
+        it "should create a new response to the next question and say thanks" do
+          post :receive_message, :params => TROPO_SMS_RESPONSE_Y
+          post :receive_message, :params => TROPO_SMS_RESPONSE_OPEN
+          post :receive_message, :params => TROPO_SMS_RESPONSE_OPEN
+          pending
+        end
+      end 
+    end
+
+    context "yn poll" do
+      before :each do
+        @poll = FactoryGirl.create(:poll_yn, :phone=>'14153334444')
+      end
+      describe "sms with valid y response" do
+        it "should create a new response" do
+          post :receive_message, :params => TROPO_SMS_RESPONSE_Y
+        end
+      end
+      describe "sms with valid yes response" do
+        pending
+      end
+      describe "sms with valid Y response" do
+        pending
+      end
+      describe "sms with valid n response" do
+        it "should creat a new response" do
+          pending
+        end
+      end
+      describe "sms with invalid response" do
+        pending "behavior to be determined"
+      end
+    end
     context "multiple choice poll" do
       before :each do
         @poll = FactoryGirl.create(:poll_multi, :phone=>'14153334444')
@@ -11,7 +80,6 @@ describe ResponsesController do
         it "should create a new response" do
           post :receive_message, :params => TROPO_SMS_RESPONSE_MULTI
           @poll.responses.length.should eq 1
-          # Response.where
         end
       end
     
