070b8d1b8de4f77c31ffe6db37cfd7ac739cde63
Alex Yule <alexy@codeforamerica.org>
2012-05-21 16:27:19 -0700

    added option.match? with passing tests

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,12 +2,17 @@ class Option < ActiveRecord::Base
   attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes
   belongs_to :question
   has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id"
-  validates_presence_of :text, :value
+  validates_presence_of :text, :value, :question_id
 
   accepts_nested_attributes_for :follow_up, :reject_if => :all_blank, :allow_destroy => true
 
   # determines if a response matched the option, with handling for yes/y/No/n 
-  def match?(response)
-    return response.text[0].downcase.match(self.value)
+  def match?(text)
+    if text[0].downcase.match(self.value)
+      return true
+    elsif text.downcase.match(self.text)
+      return true
+    end
+    false
   end  
 end
