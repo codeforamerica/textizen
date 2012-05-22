010c6cebfde45cb8a102e932e49fb6ed36e3ba6f
Alex Yule <alexy@codeforamerica.org>
2012-05-22 00:53:03 -0700

    made follow_up accessible

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,5 +1,5 @@
 class Option < ActiveRecord::Base
-  attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes
+  attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes, :follow_up
   belongs_to :question
   has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id"
   validates_presence_of :text, :value, :question_id
