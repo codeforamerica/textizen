bca5037c38a998fe52bc3c190552e55385b84bbc
Alex Yule <alexy@codeforamerica.org>
2012-05-22 18:57:27 -0700

    fixed editing bug by allowing option to destroy followups

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,7 +1,7 @@
 class Option < ActiveRecord::Base
   attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes, :follow_up
   belongs_to :question
-  has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id"
+  has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id", :dependent => :destroy
   validates_presence_of :text, :value
 
   accepts_nested_attributes_for :follow_up, :reject_if => :all_blank, :allow_destroy => true
