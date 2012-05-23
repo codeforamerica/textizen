19a629f99a107a7a6760b33ff6b2ac227ba3b2fd
Alex Yule <alexy@codeforamerica.org>
2012-05-22 18:32:28 -0700

    removed validations from option and question

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,7 +2,7 @@ class Option < ActiveRecord::Base
   attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes, :follow_up
   belongs_to :question
   has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id"
-  validates_presence_of :text, :value, :question_id
+  validates_presence_of :text, :value
 
   accepts_nested_attributes_for :follow_up, :reject_if => :all_blank, :allow_destroy => true
 
