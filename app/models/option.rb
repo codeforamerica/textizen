a025374059f6ecb0b2a48bf6348806d81fdfae0c
Alex Yule <alexy@codeforamerica.org>
2012-05-23 18:45:11 -0700

    made responses available

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,6 +2,7 @@ class Option < ActiveRecord::Base
   attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes, :follow_up
   belongs_to :question
   has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id", :dependent => :destroy
+  has_many :responses, :through => :follow_up
   validates_presence_of :text, :value
 
   accepts_nested_attributes_for :follow_up, :reject_if => :all_blank, :allow_destroy => true
