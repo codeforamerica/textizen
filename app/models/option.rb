6976a29dc0b50df69e7778a83f23d1a5e08caf5f
Alex Yule <alexy@codeforamerica.org>
2012-05-25 14:10:40 -0700

    Switched follow-ups to has_many, still many changes to make to fix this, since cocoon doesn't handle has_one well

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,7 +1,7 @@
 class Option < ActiveRecord::Base
   attr_accessible :follow_up_id, :question_id, :text, :value, :follow_up_attributes, :follow_up
   belongs_to :question
-  has_one :follow_up, :class_name => "Question", :foreign_key => "parent_option_id", :dependent => :destroy
+  has_many :follow_up, :class_name => "Question", :foreign_key => "parent_option_id", :dependent => :destroy
   has_many :follow_up_responses, :through => :follow_up, :source => :responses
   has_many :follow_up_options, :through => :follow_up, :source => :options
   validates_presence_of :text, :value
