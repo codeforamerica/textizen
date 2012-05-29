99cdd2af0bada0aacf5a98f749fbf96b97d37221
Alex Yule <alexy@codeforamerica.org>
2012-05-29 11:09:07 -0400

    fixed seeds for new schema and re-added some open responses

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,18 @@
 @q.options.create(:text=>"yes", :value=>"yes")
 @q.options.create(:text=>"no", :value=>"no")
 53.times { 
-    @resp = @q.responses.create(:from => '1'+rand(10 ** 10).to_s, :to => @p.phone, :response => ['y','n'].sample) 
-    @resp.created_at = (rand*7).days.ago
-    @resp.save
-  }
+  @resp = @q.responses.create(:from => '1'+rand(10 ** 10).to_s, :to => @p.phone, :response => ['y','n'].sample) 
+  @resp.created_at = (rand*7).days.ago
+  @resp.save
+}
 
 @p2 = Poll.create(:title=>"test poll", :phone=>'1'+rand(10 ** 10).to_s, :start_date=>Time.now, :end_date=>Time.now + 1.week)
 @p2q = @p2.questions.create(:text=>"yes or no?", :question_type=>"YN")
 @p2qy = @p2q.options.create(:text=>"yes", :value=>"yes")
-@p2qyFup = Question.create(:text=>"why", :question_type=>"OPEN")
-@p2qy.follow_up = @p2qyFup
-@p2qy.save
+@p2qyFup = @p2qy.follow_up.create( :text=>"why", :question_type=>"OPEN")
 @p2q.options.create(:text=>"no", :value=>"no")
 @p2q2 = @p2.questions.create(:text=>"where?", :question_type=>"OPEN")
+puts "creating responses for p2"
 10.times {
   @from = '1'+rand(10 ** 10).to_s
   @r = @p2q.responses.create(:from => @from, :to => @p2.phone, :response => 'yes')
@@ -30,11 +29,23 @@
   @r = @p2q.responses.create(:from => @from, :to => @p2.phone, :response => 'no')
   @r2 = @p2q2.responses.create(:from => @from, :to => @p2.phone, :response => 'everywhere')
 }  
+
+puts "creating p3"
+@op = FactoryGirl.create(:poll_open)
+@responses = ['Acme or Supreme', 'I buy groceries in Paris', 'I love Wal-mart', 'Walgreens at noon', 'CVS', 'Stater bros']
+25.times {
+  @resp = @op.questions.first.responses.create(:from => '1'+rand(10 ** 10).to_s, :to => @op.phone, :response => @responses.sample) 
+  @resp.created_at = (rand*7).days.ago
+  @resp.save
+}
+
+
+
+
 #first create one with a valid tropo phone number for later testing
 #@polls = []
 ##3.times { @polls << FactoryGirl.create(:poll)}
 #
-#@responses = ['Acme or Supreme', 'I buy groceries in Paris', 'Wal-mart', 'Walgreens', 'CVS', 'Stater bros']
 #
 #@polls.each do |p|
 #  53.times { 
@@ -73,7 +84,7 @@
 #     factory :poll_open do
 #       poll_type { 'OPEN' }
 #     end
-    
+
 #     factory :poll_ended do
 #       start_date { Time.now - 1.week }
 #       end_date { Time.now - 3.days }
