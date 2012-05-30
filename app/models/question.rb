f3f57d44655725bfee2e93dfbcf10b8e1884377f
Alex Yule <alexy@codeforamerica.org>
2012-05-30 17:14:08 -0400

    tweaks to histogram to remove whitespace

--- app/models/question.rb
+++ app/models/question.rb
@@ -38,7 +38,7 @@ class Question < ActiveRecord::Base
     puts "response histogramming time: #{responses}"
     if r.length > 0
       # create an array with all the words from all the responses
-      words = r.map{ |rs| rs.response.downcase.split(/[^A-Za-z\-]/)}.flatten
+      words = r.map{ |rs| rs.response.downcase.split(/[^A-Za-z\-]/).strip}.flatten
       unless self.options.empty?
         words.map!{ |w| self.get_matching_option(w) }
       end
