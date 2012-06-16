4f25fa546a5ac3c02d68d2ed2f4b302010a61118
Alex Yule <alexy@codeforamerica.org>
2012-06-15 18:33:22 -0700

    new group model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120606190935) do
+ActiveRecord::Schema.define(:version => 20120615230332) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "options", :force => true do |t|
     t.string   "text"
@@ -47,7 +53,7 @@ ActiveRecord::Schema.define(:version => 20120606190935) do
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      :limit => 2
+    t.integer  "month"
     t.integer  "year",       :limit => 8
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
