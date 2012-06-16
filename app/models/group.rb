11aee62c6581ab2a7efd89635fc9c9b1a9dc5073
Alex Yule <alexy@codeforamerica.org>
2012-06-15 18:33:35 -0700

    added relations to group, poll, user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ActiveRecord::Base
   attr_accessible :name
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :polls
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-  has_many :polls
+  has_many :polls, :through => :groups
+  has_many :created_polls, :class_name => "Poll", :foreign_key => "user_id"
+  has_and_belongs_to_many :groups
+
 end
