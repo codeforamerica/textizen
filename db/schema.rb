5512dc483638534f97229ea0b5b73ff5e053622d
Alex Yule <alexy@codeforamerica.org>
2012-06-20 14:34:25 -0700

    added roles to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120619013406) do
+ActiveRecord::Schema.define(:version => 20120620202954) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(:version => 20120619013406) do
     t.datetime "created_at",                                               :null => false
     t.datetime "updated_at",                                               :null => false
     t.boolean  "admin",                                 :default => false
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
