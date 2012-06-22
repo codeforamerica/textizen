1081945582b161b4966260bc212b9479ba6850e8
Alex Yule <alexy@codeforamerica.org>
2012-06-21 17:03:24 -0700

    added untested code to add users to group controller

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  attr_accessible :name, :poll_ids, :user_ids
+  attr_accessible :name, :poll_ids, :user_ids, :users
   has_and_belongs_to_many :users
   has_and_belongs_to_many :polls
   #accepts_nested_attributes_for :users, :polls
