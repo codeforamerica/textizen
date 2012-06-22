b9eb847de4049307a5868b91e3278cf81d47956c
Alex Yule <alexy@codeforamerica.org>
2012-06-22 14:10:10 -0700

    working on stubbing out the group creation form, made user_id accessible on group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  attr_accessible :name, :poll_ids
+  attr_accessible :name, :poll_ids, :user_ids
   has_and_belongs_to_many :polls
   has_many :group_users
   has_many :users, :through => :group_users
