e5787e6089c8e127b6538abcd96938e33035ffa3
Alex Yule <alexy@codeforamerica.org>
2012-06-25 21:32:36 -0700

    commented out group_user uniqueness validation, for now. getting strange error "unable to visit User"

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -2,5 +2,5 @@ class GroupUser < ActiveRecord::Base
   attr_accessible :group_id, :user_id
   belongs_to :user
   belongs_to :group
-  validates_uniqueness_of :group_id, :scope => :user
+  #validates_uniqueness_of :group_id, :scope => :user
 end
