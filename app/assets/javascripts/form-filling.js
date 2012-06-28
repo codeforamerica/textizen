e3427b21a097352a17d6e3ac7c58b164845f8df9
Joey Cody <jcody@umail.ucsb.edu>
2012-06-27 20:57:47 -0700

    hacked fix for YN and MULTI option fields

--- app/assets/javascripts/form-filling.js
+++ app/assets/javascripts/form-filling.js
@@ -1,31 +1,29 @@
 $(document).ready(function(){
-	$('.simple_form').on("change", "select.select", question_type_listener);
+	$('.simple_form').on("change", "select.select", function(event) {
+		
+		var value = $(this).val();
+		$('span.result').text('Value: ' + value); //debugging purposes
+
+		if (value == "YN") {
+			$('div.options > a.add_fields').click();
+			yn_form();
+		}
+		else if (value == "MULTI") {
+			$('div.options > a.add_fields').click();
+			multi_form();
+		}
+	});
 });
 
-function question_type_listener(event) {	
-	var value = $(this).val();
-	$('span.result').text('Value: ' + value); //debugging purposes
-	
-	if (value == "YN") {
-		$('div.options > a.add_fields').next().click(form_options(value));
-	}
-	else if (value == "MULTI") {
-		$('div.options > a.add_fields').next().click(form_options(value));
-	}
+function yn_form() {
+	$('input.string.required:first').addClass("uneditable-input").val("Yes.");
+	$('input.string.required:last').addClass("uneditable-input").val("No.");
 }
 
-
-function form_val(value) {
-	if (value == "YN")
-		$("span.result").text("YN = " + value);
-		
-	else if (value == "OPTION")
-		$("span.result").text("OPTION = " + value);
-		$('div.options > a.add_fields').next().click(form_options(value));
-	else if (value == "MULTI")
-		$("span.result").text("MULTI = " + value);
-		$('div.options > a.add_fields').next().click(form_options(value));
+function multi_form() {
+	$('input.string.required:first').val("A");
 }
+
 
 function form_options(value) {
 	//
