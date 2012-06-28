f362df6b25ac13040b8cfcd8699862538d5ff4a4
Joey Cody <jcody@umail.ucsb.edu>
2012-06-28 13:54:33 -0700

    fields all dynamically changing

--- app/assets/javascripts/form-filling.js
+++ app/assets/javascripts/form-filling.js
@@ -4,13 +4,20 @@ $(document).ready(function(){
 		var value = $(this).val();
 		$('span.result').text('Value: ' + value); //debugging purposes
 
+		// Add and remove YN response form
 		if (value == "YN") {
+			$('div.nested-fields.options').remove();
 			$('div.options > a.add_fields').click();
 			yn_form();
 		}
+		// Add and remove MULTI response form
 		else if (value == "MULTI") {
+			$('div.nested-fields.options').remove();
 			$('div.options > a.add_fields').click();
 			multi_form();
+		}
+		else if (value == "OPEN") {
+			$('div.nested-fields.options').remove();
 		}
 	});
 });
@@ -21,8 +28,10 @@ function yn_form() {
 }
 
 function multi_form() {
-	$('input.string.required:first').val("A");
-}
+	$('.control-group.string.required').html('<div class="input-prepend"><span class="add-on">A:</span><input class="span2" id="prependedInput" size="" type="text"></div>');
+	$('span.add-on:last').text('B');
+	/*$('.control-group.string.required').html('<div class="input-prepend"><span class="add-on">B:</span><input class="span2" id="prependedInput" size="" type="text"></div>');
+	*/}
 
 
 function form_options(value) {
