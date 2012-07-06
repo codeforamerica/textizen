347ba6737d9f83b2ac03b16d0d5a76a95ff75d1e
Daniel Onggunhao <danieleio@gmail.com>
2012-07-06 12:14:32 -0700

    Refactoring CSS

--- app/assets/javascripts/form_filling.js
+++ app/assets/javascripts/form_filling.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('.simple_form').on("change", "select.select", function(event) {
 
 		// Wipes existing options TODO: hide, instead of remove?
-		$(this).parents(".question-entry").children(".option_field").remove();
+		$(this).parents(".question-entry").children(".option-field").remove();
 		
 		var value = $(this).val();
 
@@ -16,8 +16,8 @@ $(document).ready(function(){
 			parent_container_entry.find(".add-option-button").click();	
 
 			// Pre-fills out information, sets class to disabled
-			parent_container_entry.find(".option_field input.string").first().addClass("disabled yes-no").val("Yes");
-			parent_container_entry.find(".option_field input.string").last().addClass("disabled yes-no").val("No");
+			parent_container_entry.find(".option-field input.string").first().addClass("disabled yes-no").val("Yes");
+			parent_container_entry.find(".option-field input.string").last().addClass("disabled yes-no").val("No");
 
 			// Removes "remove option" button
 			parent_container_entry.find(".help-inline a").remove();
