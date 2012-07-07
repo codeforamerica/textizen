033b684dcf806b1d34970fb0f14f5c6839688dc6
Alex Yule <alexy@codeforamerica.org>
2012-07-06 18:28:52 -0700

    Added confirmation field to Poll model and form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120622010048) do
+ActiveRecord::Schema.define(:version => 20120707011410) do
 
   create_table "group_users", :force => true do |t|
     t.integer  "user_id"
@@ -45,9 +45,10 @@ ActiveRecord::Schema.define(:version => 20120622010048) do
     t.string   "phone"
     t.datetime "start_date"
     t.datetime "end_date"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.integer  "user_id"
+    t.text     "confirmation"
   end
 
   create_table "questions", :force => true do |t|
