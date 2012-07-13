bdc498d4410f58c01aad693d7b98351f82ee2f02
Alex Yule <alexy@codeforamerica.org>
2012-07-12 19:52:25 -0700

    Added js and css and html with classes to fix most problems with the poll editing UI, values are now propagated correctly on both questions and followups, add-followup button is hidden and shown correctly, options have nice labels that autopopulate with the right letter, but TODO fix followup option submission bug

--- app/assets/javascripts/form_filling.js
+++ app/assets/javascripts/form_filling.js
@@ -1,27 +1,80 @@
 $(document).ready(function(){
 
+  var alphabet = ['A','B','C','D','E','F','G'];
+
   // Javascript that fills out value tag when label is filled out
 
   // TODO THIS BREAKS!
   $(document).on("click", "a.add-followup-dummy", function(event){
-    console.log("HellO!");
     $(this).parents(".option-field").find(".add-followup-button").click();
-    $(this).parents(".question-entry").addClass("has-follow-up");
+    $(this).parents(".question-entry").addClass("has-followup");
     event.preventDefault();
   });
 
-  $(".item-container").on("click", ".remove-follow-up", function (event){
-    console.log("remove follow-up");
-    $(this).parents(".question-entry").removeClass("has-follow-up");
-  });
+  // recalculate all form values
+  $(".item-container").on("insertion-callback after-removal-callback", ".question-entry", function (event){
+    var entry = $(this);
+    if (entry.find(".followup-field").length === 0){
+      entry.removeClass("has-followup");
+    }
+    // make sure this is not a yes/no question
+    if (entry.find("select.question-type").val() == "MULTI"){
+      // loop through each option text input
+      entry.find(".option-text").each(function(i,el){
+        // surround each text input with a nice bootstrappy prepend thing
+        if (!$(el).hasClass("prepended")){
+          $(el).addClass("prepended");
+          $(el).before('<div class="input-prepend"><span class="add-on option-add-on"></span>').after('</div>');
+        }
 
+        if (i == 6){
+          $(".add-option-button").hide();
+        } else if (i == 5){
+          $(".add-option-button").show();
+        }
+      });
+
+      // refresh all prepended label values
+      entry.find(".option-field .option-add-on").each(function(i, el){
+        $(el).text(alphabet[i]);
+      });
+      // refresh all hidden value inputs
+      entry.find(".option-field .option-value").each(function(i, el){
+        $(el).val(alphabet[i]);
+      });
+    }
+    if (entry.find("select.followup-type").val() == "MULTI"){
+      entry.find(".followup-option-text").each(function(i,el){
+
+        if (!$(el).hasClass("prepended")){
+          $(el).addClass("prepended");
+          $(el).before('<div class="input-prepend"><span class="add-on followup-add-on"></span>').after('</div>');
+        }
+
+        if (i == 6){
+          entry.find(".add-followup-option-button").hide();
+        } else if (i == 5){
+          entry.find(".add-followup-option-button").show();
+        }
+      });
+
+      // refresh all prepended label values
+      entry.find(".followup-option-field .followup-add-on").each(function(i, el){
+        $(el).text(alphabet[i]);
+      });
+
+      entry.find(".followup-option-field .followup-option-value").each(function(i, e){
+        $(e).val(alphabet[i]);
+      });
+    }
+  });
 
   /* Autofills the options form when a question type is selected */
   $(".simple_form").on("change", "select.question-type", function(event) {
 
     // Wipes existing options TODO: hide, instead of remove?
     $(this).parents(".nested-fields").children(".option-field").remove();
-    $(this).parents(".question-entry").removeClass("has-follow-up");
+    $(this).parents(".question-entry").removeClass("has-followup");
 
     var value = $(this).val();
 
@@ -29,14 +82,16 @@ $(document).ready(function(){
     parent_container_entry.find(".add-option-button").hide();	
 
 
-    if (value == "YN") {	
+    if (value == "YN") {
       // Clicks the add_option button twice
       parent_container_entry.find(".add-option-button").click();
       parent_container_entry.find(".add-option-button").click();	
 
       // Pre-fills out information, sets class to disabled
-      parent_container_entry.find(".option-field input.string").first().addClass("disabled yes-no").val("Yes");
-      parent_container_entry.find(".option-field input.string").last().addClass("disabled yes-no").val("No");
+      parent_container_entry.find(".option-field .option-text").first().addClass("disabled yes-no").val("Yes");
+      parent_container_entry.find(".option-field .option-value").first().addClass("disabled yes-no").val("yes");
+      parent_container_entry.find(".option-field .option-text").last().addClass("disabled yes-no").val("No");
+      parent_container_entry.find(".option-field .option-value").last().addClass("disabled yes-no").val("no");
 
       // Removes "remove option" button
       parent_container_entry.find(".help-inline a.remove_fields").remove();
@@ -45,22 +100,9 @@ $(document).ready(function(){
       // Show "add option" button
       parent_container_entry.find(".add-option-button").show();
 
-      // Find specific input to manipulate
-      var input_container_entry = $(this).parents(".option-field");
-      console.log("#1: ");
-      console.log($(this));
-      console.log("#2: ");
-      console.log($(this).parents(".option-field"));
-      // Array store of all multi values
-      var multi_vals = ["A", "B", "C", "D", "E", "F", "G"];
-
       // Clicks the add_option button twice 
       parent_container_entry.find(".add-option-button").click();
-      input_container_entry.find(".option-field input.string").before('<div class="input-prepend"><span class="add-on">' + multi_vals[0] +'</span>');
-      input_container_entry.find(".option-field input.string").after('</div>');
       parent_container_entry.find(".add-option-button").click();
-      input_container_entry.find('input.string.required').before('<div class="input-prepend"><span class="add-on">' + multi_vals[1] +'</span>');
-      input_container_entry.find('input.string.required').after('</div>');
     }
     else if (value == "OPEN") {
     }
@@ -83,9 +125,11 @@ $(document).ready(function(){
       parent_container_entry.find(".add-followup-option-button").click();	
 
       // Pre-fills out information, sets class to disabled
-      parent_container_entry.find(".followup-option-field input.string").first().addClass("disabled yes-no").val("Yes");
-      parent_container_entry.find(".followup-option-field input.string").last().addClass("disabled yes-no").val("No");
+      parent_container_entry.find(".followup-option-field .followup-option-text").first().addClass("disabled yes-no").val("Yes");
+      parent_container_entry.find(".followup-option-field .followup-option-text").last().addClass("disabled yes-no").val("No");
 
+      parent_container_entry.find(".followup-option-field .followup-option-value").first().addClass("disabled yes-no").val("yes");
+      parent_container_entry.find(".followup-option-field .followup-option-value").last().addClass("disabled yes-no").val("no");
       // Removes "remove option" button
       parent_container_entry.find(".followup-option-field a.remove_fields").remove();
     }
