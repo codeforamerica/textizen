76eb13de13e69fcb769f6561c4734360b2b96e7a
Alex Yule <alexy@codeforamerica.org>
2012-07-16 18:52:43 -0700

    Added get_exchanges method to group to fetch area codes from tropo, added collection select to group edit form. TODO: add exchange to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
+  require 'open-uri'
   attr_accessible :name, :poll_ids, :user_ids
   has_and_belongs_to_many :polls
   has_many :group_users
@@ -29,5 +30,10 @@ class Group < ActiveRecord::Base
     end
     puts errors
     return errors
+  end
+
+  def get_exchanges
+    json = open("https://api.tropo.com/v1/exchanges", :http_basic_authentication=>[ENV['TROPO_USERNAME'],ENV['TROPO_PASSWORD']]).read
+    result = JSON.parse(json).find_all{|item| item["smsEnabled"]==true}
   end
 end
