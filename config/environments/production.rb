4b2b56f1a2d17ff026f325b83ef8a7e16b3d94be
Ryan Resella <ryanresella@gmail.com>
2012-08-09 10:48:20 -0500

    Adds smtp settings to send email on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,16 @@ TxtyourcityRails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'textyourcity.herokuapp.com' }
-  
+  config.action_mailer.default_url_options = { :host => 'www.textizen.com' }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+
   # $outbound_flocky = Flocky.new ENV['FLOCKY_TOKEN'],ENV['FLOCKY_APPNUM'],{:username=>ENV['FLOCKY_USERNAME'],:password=>ENV['FLOCKY_PASSWORD']}, :queue => false
 end
