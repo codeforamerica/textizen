b85912b05cfddf65488ab6aee518a8accac66013
Alex Yule <alexy@codeforamerica.org>
2012-09-17 20:12:25 -0700

    changed action_mailer settings to save emails to file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,20 @@ TxtyourcityRails::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :file
+ # config.action_mailer.smtp_settings = {
+ #   :enable_starttls_auto => true,
+ #   :address        => "smtp.gmail.com",
+ #   :port           => 587,
+ #   :domain         => "codeforamerica.org",
+ #   :authentication => :plain,
+ #   :user_name  => "user@codeforamerica.org",
+ #   :password  => "secret"
+ # }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
